6846c2dc3ab7d1712023c84697a32325b098e00c
Ian Maurmann <ikmxx@yahoo.com>
2023-07-03 18:05:28 -0700

    Getting started on Demo App theme

--- src/packs/demo-app-theme-pack/main-layout/MainLayoutViewRequisition.php
+++ src/packs/demo-app-theme-pack/main-layout/MainLayoutViewRequisition.php
@@ -29,10 +29,32 @@ class MainLayoutViewRequisition extends PithViewRequisition
     {
         // Headers
         $this->addHeader('Use UTF-8 Encoding', 'Content-Type: text/html; charset=utf-8');
-        $this->addHeader('Show site runs PHP 8', 'X-Powered-By: PHP/8');
 
-        // Resources
+        // Preload fonts for layout
+        $this->addPreload('preload IBM Plex Sans font', '/resources/vendor/common-fonts/fontsheets/ibm-plex-sans.css', 'font-preload', 'style');
+
+        // CSS Resets for layout
         $this->addStylesheet('Fixie Reset 4 Stylesheet', '/resources/vendor/common-libraries/fixie-reset-4.0.2/fixie-reset.css', 'reset');
-        $this->addStylesheet('Green Layout Stylesheet', '/resources/framework/shared-ui/green-and-white-theme/green-and-white-theme.css', 'application-for-layout');
+
+        // CSS Libraries for layout
+        $this->addStylesheet('Font Awesome 6 free version', '/resources/vendor/common-libraries/font-awesome-free-6.4.0-web/css/all.css', 'library-for-page');
+        $this->addStylesheet('Font Awesome 4 compatibility', '/resources/vendor/common-libraries/font-awesome-4.7.0-compatibility-fork/css/font-awesome-4-compatibility-fork.min.css', 'library-for-page');
+        $this->addStylesheet('Bootstrap Icons stylesheet', '/resources/vendor/common-libraries/bootstrap-icons-font-1.10.5/font/bootstrap-icons.min.css', 'library-for-page');
+        $this->addStylesheet( 'OxCSS stylesheet',  '/resources/vendor/common-libraries/oxcss-0.1.4/dist/ox.css', 'library-for-page');
+        $this->addStylesheet('Swal2 NA stylesheet', '/resources/vendor/common-libraries/swal2-no-anthems-11.7.3/swal2-na.css', 'library-for-page');
+        $this->addStylesheet('Re-style Swal2', '/resources/vendor/common-libraries/swal2-custom-themes/swal2-aerogel-theme.css', 'library-for-page');
+        $this->addStylesheet('Hoja Aquamarine stylesheet', '/resources/vendor/common-libraries/hoja-aquamarine/hoja-aquamarine.css', 'library-for-page');
+        $this->addStylesheet('Aero-Gel stylesheet', '/resources/vendor/common-libraries/aero-gel/aero-gel.css', 'library-for-page');
+
+        // JS Libraries for layout
+        $this->addScript(     'jQuery', '/resources/vendor/common-libraries/jquery-3.6.4/jquery-3.6.4.min.js', 'library-for-page');
+        $this->addScript(     'OxCSS script','/resources/vendor/common-libraries/oxcss-0.1.4/dist/ox.js', 'library-for-page');
+        $this->addScript('Swal2 NA script', '/resources/vendor/common-libraries/swal2-no-anthems-11.7.3/swal2-na.all.js', 'library-for-page');
+
+        // App CSS for layout
+        $this->addStylesheet('Demo App Stylesheet', '/resources/theme/demo-app.css', 'application-for-layout');
+
+        // Font
+        $this->addStylesheet( 'IBM Plex Sans font', '/resources/vendor/common-fonts/fontsheets/ibm-plex-sans.css', 'font-stylesheet');
     }
 }
